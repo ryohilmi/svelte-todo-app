529a06006f87a43ef4a245788aa0a6e5404ee24f
ryohilmi <ryodm.74@gmail.com>
2022-04-29 08:03:35 +0700

    feat: add sidebar co,mponent

---
+++ src/lib/Sidebar.svelte
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<div class="sidebar">
+  <h1>Svelt<span>Do</span></h1>
+</div>
+
+<style lang="scss">
+  .sidebar {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: white;
+    height: 100vh;
+    width: 220px;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(102, 102, 102, 0.1);
+
+    h1 {
+      margin: 3rem auto;
+      text-align: center;
+      font-weight: 600;
+
+      span {
+        color: #4042c9;
+      }
+    }
+  }
+</style>
