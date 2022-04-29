64b1d00f4a5404c0045a51b6ad4d596e347b78c9
ryohilmi <ryodm.74@gmail.com>
2022-04-29 08:29:42 +0700

    feat: add day component

---
+++ src/lib/Day.svelte
@@ -0,0 +1,44 @@
+<script>
+  export let selected = false;
+  export let day;
+  export let date;
+</script>
+
+<div class="day" class:selected>
+  <p>{date}</p>
+  <p>{day}</p>
+</div>
+
+<style lang="scss">
+  @use '../style/variables' as v;
+
+  .day {
+    background: white;
+    padding: 0.5rem 0.9rem;
+    margin: 1rem;
+    text-align: center;
+    border-radius: 15px;
+    box-shadow: 0 0 5px rgba(141, 141, 141, 0.1);
+
+    &.selected {
+      background: v.$main-blue;
+
+      p {
+        color: white;
+      }
+    }
+
+    p {
+      color: #333;
+
+      &:nth-child(1) {
+        font-size: 1.1rem;
+        font-weight: 500;
+      }
+
+      &:nth-child(2) {
+        font-size: 0.7rem;
+      }
+    }
+  }
+</style>
