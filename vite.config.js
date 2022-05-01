e9301d9e1d69ce12e5368abd7778ac3c81ea85c3
ryohilmi <ryodm.74@gmail.com>
2022-05-01 20:13:32 +0700

    build: add json plugin

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import sveltePreprocess from 'svelte-preprocess';
+import json from '@rollup/plugin-json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     svelte({
       preprocess: sveltePreprocess(),
+    }),
+    json({
+      compact: true,
     }),
   ],
 });
