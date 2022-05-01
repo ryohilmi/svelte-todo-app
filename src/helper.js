3800188ebcfee4973e03741133d96b0dc0ecec0d
ryohilmi <ryodm.74@gmail.com>
2022-05-01 19:38:27 +0700

    feat: add helper functions

---
+++ src/helper.js
@@ -0,0 +1,27 @@
+export function getTodayDate() {
+  let date = new Date();
+  let dateArr = date.toString().split(' ');
+  return {
+    day: dateArr[0],
+    date: parseInt(dateArr[2]),
+    month: dateArr[1],
+  };
+}
+
+export function getDateFromToday(n) {
+  let today = new Date();
+  let date = new Date(today);
+  date.setDate(date.getDate() + n);
+  let dateArr = date.toString().split(' ');
+  return {
+    day: dateArr[0],
+    date: parseInt(dateArr[2]),
+    month: dateArr[1],
+  };
+}
+
+export function dateIsEqual(a, b) {
+  if (a.day == b.day && a.date == b.date && a.month == b.month) return true;
+
+  return false;
+}
